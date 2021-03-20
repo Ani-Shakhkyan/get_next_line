0be52f9174552874c9179711b716378b8e09f8cf
Ani <76163261+Ani-Shakhkyan@users.noreply.github.com>
2021-03-20 17:47:33 +0400

    Add files via upload
    final_my_version

--- get_next_line.c
+++ get_next_line.c
@@ -1,123 +1,67 @@
+#include <stdio.h>
+#include "get_next_line.h"
+#include <fcntl.h>
 #include <unistd.h>
-int		ft_isprint(int c)
-{
-	return (c >= 32 && c <= 126);
-}
 
-size_t	ft_strlen(const char *s)
-{
-	size_t i;
-
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	size_t	len;
-	size_t	i;
-	size_t	j;
-	char	*join_str;
-
-	if (!s1 || !s2)
-		return (NULL);
-	len = ft_strlen(s1) + ft_strlen(s2);
-	if (!(join_str = malloc(sizeof(*s1) * len + 1)))
-		return (NULL);
-	i = 0;
-	j = 0;
-	while (s1[j])
-	{
-		join_str[i++] = s1[j++];
-	}
-	j = 0;
-	while (s2[j])
-	{
-		join_str[i++] = s2[j++];
-	}
-	join_str[i] = "\0";
-	return (join_str);
-}
 char *is_newline(char *str)
 {
 	size_t a;
 
 	a = 0;
-	while((str + a))
+	while(*(str + a))
 	{
-		if((str + a) == "\n");
+		if(*(str + a) == '\n')
 			return (str + a);
 		++a;
 	}
 	return (NULL);
 }
 
-char	*ft_strdup(const char *s1)
-{
-	char	*cpy;
-	char	*s1_cpy;
-	int		i;
-	int		size;
-
-	i = 0;
-	size = 0;
-	s1_cpy = (char*)s1;
-	while (s1_cpy[size])
-		size++;
-	if (!(cpy = (char*)malloc(sizeof(char) * size + 1)))
-		return (NULL);
-	while (s1_cpy[i])
-	{
-		cpy[i] = s1_cpy[i];
-		i++;
-	}
-	cpy[i] = '\0';
-	return (cpy);
-}
-char *till_nl(char *str)
-{
-	int i;
-	char *strcp;
-	i = 0;
-	while((str + i) != "\n")
-		i++;
-	*strcp = (char*)malloc(i + 1);
-	*strcp(i + 1) = "/0"
-	while(i)
-	{
-		strcp(i) = str(i);
-	}
-}
-	char *get_line()
-{
-	static char *read;
-	char buf = [BUFFER_SIZE + 1];
-	char *boo;
-	char *line;
-
-	read(fd, buf, BUFFER_SIZE + 1);
-	boo = is_newline(*buf);
-	if(boo)
-	{
-		if(read)
-		{
-			line = ft_strjoin(read,till_nl);
-		}
-		else 
-			line = ft_strdup(buff);
-
-	}
-		
-}
 
 int  get_next_line(int fd, char **line)
 {
-	static char *read;
+	static char *remaining;
 	char *buf;
-	char *line;
-	read(fd, buf, BUFFER_SIZE)
-	
+	int indicator;
+	char *nl;
+	int i;
 
+	i = 0;
+	if (fd < 0 || !line || ((BUFFER_SIZE + 1) <= 0))
+		return (-1);
+	if (!(buf = malloc(sizeof(char) * (BUFFER_SIZE + 1))))
+		return (-1);
+	while((indicator = read(fd, buf, BUFFER_SIZE + 1)) > 0)
+	{
+
+	write(1, "Hello", 1);
+		buf[indicator] = '\0';
+		if(is_newline(buf))
+		{
+			while(buf[indicator] != '\n')
+			{
+				remaining[i] = buf[--indicator];
+				i++;
+			}
+			i = 0;
+			*line[i + 1] = '\0';
+			while(indicator >= 0){
+				*line[i++] = buf[indicator--];
+			}
+			if(*line)
+			{
+				return 1;
+			}
+			free(buf);
+		}		
+	}
+	return (1);	
+}
+
+int		main()
+{
+	int	fd;
+	char *line;
+	fd = open("./text.txt", O_RDONLY);
+	get_next_line(fd, &line);
 }
